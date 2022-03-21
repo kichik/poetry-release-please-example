4a44c6916b5b84a617785542292256e119bba0c6
Amir Szekely <kichik@gmail.com>
2022-03-21 15:27:52 -0700

    feat: Some actual code

---
+++ src/hello.py
@@ -0,0 +1,15 @@
+import importlib.metadata
+
+import requests
+
+
+def main():
+    try:
+        print("version", importlib.metadata.version("poetry-release-please-example"))
+    except importlib.metadata.PackageNotFoundError:
+        print("package not installed")
+    print("my ip", requests.get("https://api.ipify.org").text)
+
+
+if __name__ == "__main__":
+    main()
